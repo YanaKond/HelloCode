a0fc546af0f115ef13eb39f75ac5c4ddc4ac940c
Yana Kond <https://github.com/YanaKond>
2023-11-19 17:07:02 +0300

    add Example 010 Method Array

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int [] array = {17, 24, 65, 57, 89, 5431,674, 32};
+
+int n = array.Length;
+int find = 24;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+
+}
